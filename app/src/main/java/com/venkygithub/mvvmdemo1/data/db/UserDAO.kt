4f66c6d99b2b0da4618a17911636f684ec491b19
venkateshn <naiduvenkatesh18@yahoo.in>
2020-03-02 13:03:53 +0530

    Room components(DB+Entity+DAO)

---
+++ app/src/main/java/com/venkygithub/mvvmdemo1/data/db/UserDAO.kt
@@ -0,0 +1,20 @@
+package com.venkygithub.mvvmdemo1.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.venkygithub.mvvmdemo1.data.db.entities.CURRENT_USER_ID
+import com.venkygithub.mvvmdemo1.data.db.entities.User
+
+@Dao
+interface UserDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User) : Long
+
+    @Query("SELECT * FROM user_table WHERE uid = $CURRENT_USER_ID")
+    fun getUser() : LiveData<User>
+
+}
